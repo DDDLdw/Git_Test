ca55b87d8084eae4f1f9332db0ebc84ef585d17a
ldw <2227036748@qq.com>
2021-07-17 11:18:14 +0800

    test1 commit

--- src/main/java/com/ldw/Test.java
+++ src/main/java/com/ldw/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("father");
         System.out.println("mother");
+        System.out.println("[[[[[[[[[");
     }
 }
