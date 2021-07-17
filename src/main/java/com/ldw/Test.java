0889b359793242c62bdf18f40054991b4a858484
ldw <2227036748@qq.com>
2021-07-17 11:04:20 +0800

    first commit

---
+++ src/main/java/com/ldw/Test.java
@@ -0,0 +1,12 @@
+package com.ldw;
+
+/**
+ * @Author liduwei
+ * @Date 2021/7/17 10:55
+ * @Param
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("father");
+    }
+}
